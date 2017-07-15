b7c8b783a4a1a9ae4bd0b94aed58ad3e482a28e5
GeLinfei <651384398@11.com>
2017-07-15 21:33:32 +0800

    backup

--- UnitTest.bat
+++ UnitTest.bat
@@ -1,6 +1,6 @@
-cd /d C:\Users\linfe\LargeScaleLogSystem\UnitTest
+cd /d D:\git\LargeScaleLogSystem\UnitTest
 echo start UnitTest
-start "UnitTest" /D "C:\Users\linfe\LargeScaleLogSystem" npl -d bootstrapper="UnitTest.lua" servermode="true" dev="../../" raftMode="server" baseDir="" mpPort="8088"
+start "UnitTest" /D "D:\git\LargeScaleLogSystem" npl -d bootstrapper="UnitTest.lua" servermode="true" dev="../../" raftMode="server" baseDir="" mpPort="8088"
 
 pause
 
